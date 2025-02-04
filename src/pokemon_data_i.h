07aab98465cd486a301a1bea9097b9fae99ed6f6
Kris Bahnsen <Kris@KBEmbedded.com>
2025-02-03 20:16:04 -0800

    pokemon_data_i: add some notes
    Signed-off-by: Kris Bahnsen <Kris@KBEmbedded.com>

--- src/pokemon_data_i.h
+++ src/pokemon_data_i.h
@@ -1,13 +1,87 @@
 #ifndef __POKEMON_DATA_I_H__
 #define __POKEMON_DATA_I_H__
 
-#include <src/include/pokemon_data.h>
+/* Creat a uniform, generation independent struct to represent the current pokemon
+ * in the Flipper's memory. These are then copied into a struct that is laid out
+ * in memory for the target trade generation. Upon receiving a traded pokemon,
+ * that struct is back-copied into this struct.
+ *
+ * This takes up additional memory to keep multiple copies like that, but
+ * greatly reduces code complexity and potential mistakes, and likely reduces
+ * code size far more than this adds memory pressure. We also need to track
+ * far fewer bytes as many of the final memory values are calculated.
+ */
+/* XXX: This is maintained in flipper endianness! */
+/* XXX: Need to include all of the variables that could ever be in any trade
+ * struct, methinks...
+ */
+struct __attribute__((__packed__)) pokemon_info {
+	uint8_t index;
+	uint32_t exp;
+	//uint32_t exp;
+	char ot_name[LEN_NAME_BUF];
+	char trainer_name[LEN_NAME_BUF];
+	char nickname[LEN_NAME_BUF];
+	/* XXX: Do we want to track HP and max HP separately?
+	 * Why? is there some benefit to keeping that information?
+	 * On the one hand, it would allow us to keep exact copies of what
+	 * was traded to us. On the other, but why?
+	 */
+	uint16_t hp;
+	uint16_t max_hp;
+	/* Level is normally calculated from exp, however, level is more human
+	 * readable compared to just exp. Therefore, we only let the user set
+	 * the level and then back calculate exp from that as well as Max HP,
+	 * ATK, DEF, SPD, SPC.
+	 */
+	uint8_t level;
+	/* Type is only something can can be modified in gen I, and even then
+	 * any moves that modify type (transform, etc.) will clobber these later.
+	 */
+	/* XXX: We might also want to track exp separately, that way if we're
+	 * traded in a pokemon, we don't lose the level and recalculate it all.
+	 */
+	uint8_t type[2];
+	/* catch_held has multiple purposes depending on generation. However,
+	 * we really only use it for held item in gen II. In gen I, this value
+	 * is the catch rate, which, when transferring between generations can
+	 * leave the pokemon holding a random item equivalent to its catch rate.
+	 */
+	uint8_t catch_held;
+	uint8_t move[4];
+	uint16_t ot_id;
+	uint16_t hp_ev;
+	uint16_t atk_ev;
+	uint16_t def_ev;
+	uint16_t spd_ev;
+	uint16_t spc_ev;
+	/* XXX: TODO: Is there a more clean way to handle this?
+	 * Could use union with struct+bitfields and a single var
+	 * Could copy
+	 * Could eliminate the use of single IV value gathering which would 
+	 *   complicate other code that grabs the IV value and does manipulation
+	 *   from that.
+	 *
+	 * Also note that these are currently for gen I and gen II, gen III and
+	 * onward use a different IV system
+	 */
+	uint8_t atk_iv;
+	uint8_t def_iv;
+	uint8_t spd_iv;
+	uint8_t spc_iv;
+	uint8_t pokerus;
 
-//#include <src/pokemon_app.h>
-//#include <src/include/pokemon_char_encode.h>
-
-static void pokemon_stat_ev_calc(PokemonData* pdata, EvIv val);
-static void pokemon_stat_iv_calc(PokemonData* pdata, EvIv val);
+	/* Right now, these values are calculated from exp, et al., it may make sense
+	 * to instead JIT calculate when going from this struct to the real
+	 * trade block. However, all of our calculation code is expecting
+	 * to set the data live to the trade block.
+	 */
+	uint16_t atk;
+	uint16_t def;
+	uint16_t spd;
+	uint16_t spc_atk;
+	uint16_t spc_def;
+};
 
 /* The struct is laid out exactly as the data trasfer that gets sent for trade
  * information. It has to be packed in order to not have padding in the Flipper.
@@ -69,6 +143,8 @@ struct __attribute__((__packed__)) trade_block_gen_i {
     uint8_t party_members[7];
     /* Only the first pokemon is set up, even though there are 6 total party members */
     PokemonPartyGenI party[6];
+    /* XXX: TODO: Instead of having an array of 6 party members, have a single party
+     * and then 5 more "dead" party entries*/
     /* Only the first pokemon has an OT name and nickname even though there are 6 members */
     /* OT name should not exceed 7 chars! */
     Name ot_name[6];
@@ -93,8 +169,10 @@ struct __attribute__((__packed__)) pokemon_party_data_gen_ii {
     uint16_t spc_ev;
     uint16_t iv;
     uint8_t move_pp[4];
+    /* After a trade, friendship is always reset to 120, 70?, no use changing it */
     uint8_t friendship;
     uint8_t pokerus;
+    /* Only used in Crystal, not worth implementing unless someone really wants it */
     uint16_t caught_data;
     /* Level is normally calculated from exp, however, level is more human
      * readable/digestable compared to exp. Therefore, we set level and then
@@ -134,6 +212,8 @@ struct __attribute__((__packed__)) trade_block_gen_ii {
     uint16_t trainer_id;
     /* Only the first pokemon is set up, even though there are 6 total party members */
     PokemonPartyGenII party[6];
+    /* XXX: TODO: Instead of having an array of 6 party members, have a single party
+     * and then 5 more "dead" party entries*/
     /* Only the first pokemon has an OT name and nickname even though there are 6 members */
     /* OT name should not exceed 7 chars! */
     Name ot_name[6];
