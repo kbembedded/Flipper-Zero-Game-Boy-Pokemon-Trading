a6e93a502397bb710dc91c288a74c950dc50b715
Kris Bahnsen <Kris@KBEmbedded.com>
2026-05-26 20:53:56 -0700

    stats: migrate enum to X macro pattern for less fragility
    Signed-off-by: Kris Bahnsen <Kris@KBEmbedded.com>

--- src/include/stats.h
+++ src/include/stats.h
@@ -3,85 +3,82 @@
 
 #pragma once
 
+/* NOTE: ATK should be first, if not, the enum needs to be adjusted too! */
+#define POKEMON_STAT(X) \
+	X(ATK)		\
+	X(DEF)		\
+	X(SPD)		\
+	X(SPC)		\
+	X(SPC_ATK)	\
+	X(SPC_DEF)	\
+	X(HP)
+
 typedef enum {
-    /* Base stats */
-    STAT_BASE = 0, // Sentry value
-    STAT_BASE_ATK = 0,
-    STAT_BASE_DEF,
-    STAT_BASE_SPD,
-    /* NOTE! While accessing SPC/APC_AT will do the correct thing for both
-     * Gen I and Gen II, accessing SPC_DEF for Gen I will return a value
-     * that is not used in Gen I games. This normally isn't an issue, but
-     * is a potential gotcha to be aware of.
-     */
-    STAT_BASE_SPC,
-    STAT_BASE_SPC_ATK,
-    STAT_BASE_SPC_DEF,
-    STAT_BASE_HP,
-    STAT_BASE_TYPE,
-    STAT_BASE_MOVE,
-    STAT_BASE_GROWTH,
-    STAT_BASE_GENDER_RATIO,
-    STAT_BASE_INDEX,
-    STAT_BASE_END, // Sentry value
+	/* Main stats */
+#define X(name) STAT_##name,
+	POKEMON_STAT(X)
+#undef X
+	STAT_END,
 
-    /* In-party stats */
-    STAT = 0, // Sentry value
-    STAT_ATK = 0,
-    STAT_DEF,
-    STAT_SPD,
-    /* Gen I uses SPC, Gen II uses SPC_ATK and SPC_DEF */
-    STAT_SPC,
-    STAT_SPC_ATK,
-    STAT_SPC_DEF,
-    STAT_HP,
-    STAT_END = 7, // Sentry value
-    STAT_TYPE = 7,
-    /* Move is left here to line up with the table base stats */
-    STAT_MOVE,
+	/* Set STAT_OFFS manually to STAT_ATK for clean and consistent use of
+	 * enum values for looping.
+	 */
+	STAT_OFFS = STAT_ATK,
 
-    STAT_EV = 10, // Sentry value
-    STAT_EV_OFFS = 10,
-    STAT_ATK_EV = 10,
-    STAT_DEF_EV,
-    STAT_SPD_EV,
-    /* There is only SPC EV, there is no SPC_ATK/DEF. However, for the sake
-     * of making calculations easier, we pretend there are. This means that
-     * SPC/SPC_ATK/SPC_DEF calculations all grab the SPC EV.
-     */
-    STAT_SPC_EV,
-    STAT_SPC_ATK_EV,
-    STAT_SPC_DEF_EV,
-    STAT_HP_EV = 16,
-    STAT_EV_END, // Sentry value
+	/* Effort Value (EV) stats */
+	STAT_EV_OFFS = STAT_END,
+#define X(name) STAT_##name##_EV = STAT_##name + STAT_EV_OFFS,
+	POKEMON_STAT(X)
+#undef X
+	STAT_EV_END,
 
-    STAT_IV = 19,
-    STAT_IV_OFFS = 20, // Sentry value
-    STAT_ATK_IV = 20,
-    STAT_DEF_IV,
-    STAT_SPD_IV,
-    /* There is only SPC IV, there is no SPC_ATK/DEF. However, for the sake
-     * of making calculations easier, we pretend there are. This means that
-     * SPC/SPC_ATK/SPC_DEF calculations all grab the SPC IV.
-     */
-    STAT_SPC_IV,
-    STAT_SPC_ATK_IV,
-    STAT_SPC_DEF_IV,
-    STAT_HP_IV = 26,
-    STAT_IV_END, // Sentry value
+	/* Individual Value (IV) stats
+	 * Note that in Gen I and II, IV is a 16 bit value split in to four
+	 * nibbles. The pokemon data info structure is set up with the correct
+	 * endianness for these generations to access as a single value, as well
+	 * as a union of bitfields.
+	 *
+	 * All of that is to say, most general purpose access should happen
+	 * through individual IVs, rather than the whole IV. And then the
+	 * translation to and from trade block format would grab the whole IV.
+	 */
+	STAT_IV_OFFS = STAT_EV_END,
+#define X(name) STAT_##name##_IV = STAT_##name + STAT_IV_OFFS,
+	POKEMON_STAT(X)
+#undef X
+	STAT_IV_END,
 
-    /* These won't ever really be needed in groups */
-    STAT_LEVEL = 28,
-    STAT_NUM,
-    STAT_CONDITION,
-    STAT_NICKNAME,
-    STAT_OT_NAME,
-    STAT_OT_ID,
-    STAT_TRAINER_NAME,
-    STAT_SEL, // which EV/IV calc to use
-    STAT_EXP,
-    STAT_HELD_ITEM,
-    STAT_POKERUS,
+	STAT_BASE_OFFS = STAT_IV_END,
+#define X(name) STAT_BASE_##name = STAT_##name + STAT_BASE_OFFS,
+	POKEMON_STAT(X)
+#undef X
+	/* NOTE! While accessing SPC/APC_AT will do the correct thing for both
+	 * Gen I and Gen II, accessing SPC_DEF for Gen I will return a value
+	 * that is not used in Gen I games. This normally isn't an issue, but
+	 * is a potential gotcha to be aware of.
+	 */
+	STAT_BASE_TYPE,
+	STAT_BASE_MOVE,
+	STAT_BASE_GROWTH,
+	STAT_BASE_GENDER_RATIO,
+	STAT_BASE_INDEX,
+	STAT_BASE_END,
+
+	/* Additional live stats that arn't grouped with the above. */
+	STAT_TYPE,
+	STAT_MOVE,
+	STAT_IV,
+	STAT_LEVEL,
+	STAT_NUM,
+	STAT_CONDITION,
+	STAT_NICKNAME,
+	STAT_OT_NAME,
+	STAT_OT_ID,
+	STAT_TRAINER_NAME,
+	STAT_SEL,
+	STAT_EXP,
+	STAT_HELD_ITEM,
+	STAT_POKERUS,
 } DataStat;
 
 typedef enum {
